736f50f6120dd6681762b0f1702d245c38b7b834
고민호 <rhalsgh934@naver.com>
2018-07-13 17:23:49 +0900

    j

--- 05_Spring & Mybatis/src/main/webapp/index.jsp
+++ 05_Spring & Mybatis/src/main/webapp/index.jsp
@@ -2,6 +2,11 @@
     pageEncoding="UTF-8"%>
     
     <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -21,9 +26,9 @@
 </script>
 <c:if test="${empty sessionScope.member.userId}">
 <form action="/login.do" method="post">
-Id:<input type="text" name="userId">
-PW:<input type="text" name="userPw">
-<input type="submit" value="로그인">
+Id:<input type="text" class="form-control" name="userId">
+PW:<input type="text" class="form-control" name="userPw">
+<input type="submit" class="btn btn-primary" value="로그인">
 <a href="/enrollPage.do">회원가입</a>
 </form>
 </c:if>
---
+++ android_web/src/tset/controller/TestServlet.java
@@ -0,0 +1,106 @@
+package tset.controller;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet(name = "Test", urlPatterns = { "/test" })
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("파파고 호출");
+		request.setCharacterEncoding("utf-8");
+		String data=request.getParameter("data");
+		System.out.println(data);
+		
+		String json=testPaPaGo(data);
+		
+		System.out.println(json);
+		response.setContentType("text/html; charset=utf-8");
+		
+		response.getWriter().println(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	public String testPaPaGo(String srcData) {
+		 String clientId = "UggLdq0vK8I9NkETHXmI";//애플리케이션 클라이언트 아이디값";
+	        String clientSecret = "auGeQVrZoX";//애플리케이션 클라이언트 시크릿값";
+	        try {
+	            String text = URLEncoder.encode(srcData, "UTF-8");
+	            String apiURL = "https://openapi.naver.com/v1/papago/n2mt";
+	            URL url = new URL(apiURL);
+	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	            con.setRequestMethod("POST");
+	            con.setRequestProperty("X-Naver-Client-Id", clientId);
+	            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+	            // post request
+	            String postParams = "source=ko&target=en&text=" + text;
+	            con.setDoOutput(true);
+	            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+	            wr.writeBytes(postParams);
+	            wr.flush();
+	            wr.close();
+	            int responseCode = con.getResponseCode();
+	            BufferedReader br;
+	            if(responseCode==200) { // 정상 호출
+	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	            } else {  // 에러 발생
+	                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	            }
+	            String inputLine;
+	            StringBuffer response = new StringBuffer();
+	            while ((inputLine = br.readLine()) != null) {
+	                response.append(inputLine);
+	            }
+	            br.close();
+	           System.out.println(response.toString());
+	            
+
+	            
+	            return response.toString();
+	            
+	            
+	          
+	            
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+	        return null;
+	}
+
+}
